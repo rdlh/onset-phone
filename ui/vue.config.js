a07486c23cfd76ad56d898642565ff8c67742f0f
Remi Delhaye <contact@rdlh.io>
2019-12-18 22:31:54 +0100

    Remove hash from assets

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  runtimeCompiler: true
+  runtimeCompiler: true,
+  chainWebpack: config => {
+    if(config.plugins.has('extract-css')) {
+      const extractCSSPlugin = config.plugin('extract-css')
+      extractCSSPlugin && extractCSSPlugin.tap(() => [{
+        filename: '[name].css',
+        chunkFilename: '[name].css'
+      }])
+    }
+  },
+  configureWebpack: {
+    output: {
+      filename: '[name].js',
+      chunkFilename: '[name].js'
+    }
+  }
 }
